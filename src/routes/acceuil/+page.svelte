814b693157b6226ffa323fe00aeeeb92ee951ee8
Mehdi Sassoui <mehdi.sassoui@student.uclouvain.be>
2024-04-09 15:31:31 +0200

    corr

--- src/routes/acceuil/+page.svelte
+++ src/routes/acceuil/+page.svelte
@@ -1,45 +1,12 @@
 <script>
-  import {onMount} from 'svelte';
-  import user from '../user';
-  $: isLoggedIn = $user === null? false : true;
-
-  onMount(async()=>{
-      const userLoggedInStatus = async()=>{
-          const result = await fetch('http://localhost:3030/api/user',{
-              method: 'GET',
-              credentials: 'include',
-              headers: {
-                  'Accept': 'application/json',
-                  'content-type': 'application/json'
-              }
-          })
-          return result
-      }
-      const result = await userLoggedInStatus();
-      const returnedData = await result.json();
-      if(returnedData.success === true) user.update(val => val = returnedData.data)
-  })
-
-
-
+  // Add your script logic here
 </script>
 
-<div class="content_wrapper">
-  <h1>Welcome to Consulting Ninja</h1>
-  {#if isLoggedIn}
-  <h2>Thank you {$user.firstname} {$user.lastname} for logging in!</h2>
-  {:else}
-  <h2>You need to be logged in here.</h2>
-  <h2>Head to Sign in page or Register if you don't have an account.</h2>
-  {/if}
-</div>
-
 <style>
-  h1, h2{
-      color: #FFF;
-  }
-  .content_wrapper{
-      margin-top: 4em;
-      text-align: center;
-  }
+  /* Add your styles here */
 </style>
+
+<main>
+  <h1>Welcome to the Homepage!</h1>
+  <p>This is a simple Svelte homepage.</p>
+</main>
